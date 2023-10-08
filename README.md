8145ab36321104258c50fa6a8f9b90837cabe6e1
dariogarcia <dario.garcia@bsc.es>
2023-10-08 23:26:00 +0200

    Expands k-means, allows setting a label. Takes input params now.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # fcb_player_ratings
 Analysis of FCB player ratings, gathered from RAC1.
 
-MIT License
+# To Dos
+Add column for player position, and create matrices aggregated by this feature. Analysis with regards of position, not player.
+
+This software is released under the MIT License.
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
@@ -8,7 +9,7 @@ from sklearn.decomposition import PCA
 # Function to read the original data matrix
 def read_data_matrix(file_path):
     try:
-        data = pd.read_csv(file_path, delimiter=',', index_col=0)
+        data = pd.read_csv(file_path, delimiter=',', index_col=False)
         data = data.fillna(0)  # Fill NaN values with 0
         return data.values, data.columns
     except FileNotFoundError:
@@ -54,22 +55,37 @@ def to_transactions(data, item_names):
     return transactions
 
 # Clustering
-def run_kmeans(data, num_clusters):
-    kmeans = KMeans(n_clusters=num_clusters, random_state=0)
+def run_kmeans(data, num_clusters, labels=None):
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0, n_init=20)
     clusters = kmeans.fit_predict(data)
 
+    # Define a colormap with a fixed number of colors
+    colormap = plt.cm.get_cmap('Dark2_r', num_clusters)
     # Reduce dimensionality for plot
     pca = PCA(n_components=2)
     reduced_data = pca.fit_transform(data)
+    cluster_centers = kmeans.cluster_centers_
     # Visualize the clusters
     plt.figure(figsize=(8, 6))
-    plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=clusters, cmap='viridis')
+
+    if labels is not None:
+        symbols = ['o', '*', 's', '^', 'D', 'v', 'p', 'H', '+']
+        unique_labels = np.unique(labels)
+        for i, label in enumerate(unique_labels):
+            label_indices = np.where(labels == label)
+            scatter = plt.scatter(reduced_data[label_indices, 0], reduced_data[label_indices, 1],
+                        label=f'Label {label}', c=colormap(i % num_clusters), marker=symbols[i % len(symbols)])
+    
+    else:
+        scatter = plt.scatter(reduced_data[:, 0], reduced_data[:, 1], c=clusters, cmap=colormap)
     plt.title('K-Means Clustering')
     plt.xlabel('Feature 1')
     plt.ylabel('Feature 2')
+    # Create a legend with cluster labels
+    legend_labels = [f'Cluster {i}' for i in range(num_clusters)]
+    legend = plt.legend(handles=scatter.legend_elements()[0], labels=legend_labels)
     plt.colorbar(label='Cluster')
     plt.show()
-    print(clusters)
     return
 
 # Itemsets
@@ -83,21 +99,37 @@ def run_apriori(data, min_support = 0.34, min_confidence = 0.5):
         print(rule)
     return
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Perform data analysis on FCB player ratings.')
+    parser.add_argument('file_path', type=str, help='Path to the input CSV file')
+    parser.add_argument('--label', type=str, choices=['date', 'outcome', 'competition'], default=None,
+                        help='Column to use as label (default: None)')
+    parser.add_argument('--data', type=str, choices=['player_bool', 'player_sum'], default='player_bool',
+                        help='Column to use as data (default: player_bool)')
+    return parser.parse_args()
+
 
 # Main
 if __name__ == "__main__":
-    file_path = "../data/data_raw.csv"
+    args = parse_arguments()
+    file_path = args.file_path
     original_data, original_item_names = read_data_matrix(file_path)
-    
-    #Compute derived matrix
-    voted_data, voted_labels = derive_voted(original_data, original_item_names)
-    sum_votes_data, voted_labels = derive_sum_votes(original_data, original_item_names)
-
-    transactions = to_transactions(voted_data, voted_labels)
+    label_column = None
+    data_column = None
+    if args.label == 'date':
+        label_column = original_data[:, 0]  # Use the date column as labels
+    elif args.label == 'outcome':
+        label_column = original_data[:, 1]  # Use the outcome column as labels
+    elif args.label == 'competition':
+        label_column = original_data[:, 2]  # Use the competition column as labels
+    if args.data == 'player_bool':
+        data_columns, row_labels = derive_voted(original_data[:,3:], original_item_names[3:])
+    elif args.data == 'player_sum':
+        data_columns, row_labels = derive_sum_votes(original_data[:,3:], original_item_names[3:])
+    #Run Apriori
+    transactions = to_transactions(data_columns, row_labels)
     print("\nTransaction List:")
     print(transactions)
-    #Run Apriori
     run_apriori(transactions)
-
     #Run K-means
-    run_kmeans(sum_votes_data, 3)
+    run_kmeans(data_columns, 3, label_column)
